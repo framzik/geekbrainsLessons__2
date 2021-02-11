f094512d1fe5dc0fc446f7ca461b8075494bd0bc
framzik <faradei33>
2021-02-11 14:39:52 +0300

    Необходимо написать два метода, которые делают следующее: 1) Создают одномерный длинный массив, например: 2) Заполняют этот массив единицами. 3) Засекают время выполнения: long a = System.currentTimeMillis(). 4) Проходят по всему массиву и для каждой ячейки считают новое значение 5) Проверяется время окончания метода System.currentTimeMillis(). 6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a).

---
+++ src/main/java/ru/khrebtov/Lesson5/Main.java
@@ -0,0 +1,65 @@
+package ru.khrebtov.Lesson5;
+
+public class Main {
+  private static int SIZE = 10000000;
+
+  public static void main(String[] args) {
+
+    notMultithreadedMethod();
+    new Thread(() -> multithreadedMethod()).start();
+//    new Thread(() -> multithreadedMethod()).start();
+  }
+
+  public static void notMultithreadedMethod() {
+    float[] arr = new float[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+      arr[i] = 1;
+    }
+    long startTime = System.currentTimeMillis();
+    subtract(arr);
+    long endTime = System.currentTimeMillis();
+    System.out.println("Время выполнения без многопоточности:" + (endTime - startTime));
+  }
+
+  public static void multithreadedMethod() {
+    int half = SIZE / 2;
+    float[] arr = new float[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+      arr[i] = 1;
+    }
+    long startTime = System.currentTimeMillis();
+
+    float[] halfArr1 = new float[half];
+    System.arraycopy(arr, 0, halfArr1, 0, half);
+    float[] halfArr2 = new float[half];
+    System.arraycopy(arr, half, halfArr2, 0, half);
+
+    synchronized (halfArr1) {
+      subtract(halfArr1);
+      System.arraycopy(halfArr1, 0, arr, 0, half);
+    }
+    synchronized (halfArr2) {
+      subtract(halfArr2);
+      System.arraycopy(halfArr2, 0, arr, half, half);
+    }
+
+    long endTime = System.currentTimeMillis();
+    System.out.println("Время выполнения с многопоточностью:" + (endTime - startTime));
+  }
+
+  private static void subtract(float[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    }
+  }
+
+  private static void printArr(float[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+
+}
+
+
